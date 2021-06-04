92d8e04309467caff4cecffeafee1804ff2b8e8d
Felipe Santarosa <snowychest@gmail.com>
2021-06-04 06:53:12 -0300

    fix: Update constants file so they can have types and other useful keys

--- src/constants/validButtons.js
+++ src/constants/validButtons.js
@@ -1,23 +1,29 @@
 export const numbers = [
-  {display: '1'},
-  {display: '2'},
-  {display: '3'},
-  {display: '4'},
-  {display: '5'},
-  {display: '6'},
-  {display: '7'},
-  {display: '8'},
-  {display: '9'},
+  {display: '1', type: 'number'},
+  {display: '2', type: 'number'},
+  {display: '3', type: 'number'},
+  {display: '4', type: 'number'},
+  {display: '5', type: 'number'},
+  {display: '6', type: 'number'},
+  {display: '7', type: 'number'},
+  {display: '8', type: 'number'},
+  {display: '9', type: 'number'},
 ];
 
-export const mathOperations = [{display: '+'}, {display: '-'}, {display: '/'}, {display: 'x'}, {display: '='}];
+export const mathOperations = [
+  {display: '+', type: 'mathOperation', relativeOperator: '+'},
+  {display: '-', type: 'mathOperation', relativeOperator: '-'},
+  {display: '÷', type: 'mathOperation', relativeOperator: '/'},
+  {display: 'x', type: 'mathOperation', relativeOperator: '*'},
+  {display: '=', type: 'mathOperation', relativeOperator: '='},
+];
 
 export const consoleOperations = [
   {display: 'CE', operation: () => {}},
   {
     display: 'C',
     operation: (setPrompt) => {
-      setPrompt({value: 0, hasJustStarted: true});
+      setPrompt({value: '0', hasJustStarted: true});
     },
   },
   {
@@ -26,7 +32,7 @@ export const consoleOperations = [
       setPrompt((prevPrompt) => {
         if (prevPrompt.value.length > 1) return {...prevPrompt, value: prevPrompt.value.slice(0, -1)};
 
-        if (prevPrompt.value > 0) return {...prevPrompt, value: 0, hasJustStarted: true};
+        if (parseInt(prevPrompt.value) > 0) return {...prevPrompt, value: '0', hasJustStarted: true};
 
         return prevPrompt;
       });
